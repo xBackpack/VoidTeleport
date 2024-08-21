d81d464653f184a9c56af9d0ba22e3e2fdf62a3d
xBackpack <jamescheeseman21@gmail.com>
2024-08-21 13:05:55 +0100

    Initial commit

--- src/main/kotlin/me/xbackpack/voidTeleport/VoidTeleport.kt
+++ src/main/kotlin/me/xbackpack/voidTeleport/VoidTeleport.kt
@@ -5,10 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin
 class VoidTeleport : JavaPlugin() {
 
     override fun onEnable() {
-        // Plugin startup logic
-    }
 
-    override fun onDisable() {
-        // Plugin shutdown logic
+
+
     }
 }
